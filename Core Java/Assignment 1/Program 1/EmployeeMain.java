65fc2782ebbccdd8d14892609edfb5821c57b69d
Shounak Lohokare <shounaklohokare@gmail.com>
2022-11-30 15:23:51 +0530

    edit

--- Core Java/Assignment 1/Program 1/EmployeeMain.java
+++ Core Java/Assignment 1/Program 1/EmployeeMain.java
@@ -1,47 +1,48 @@
 import java.io.*;
+import Employee;
 
 public class EmployeeMain {
-	
-	public static void main(String[] args) throws IOException{
-		
+
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		System.out.print("\nEnter the number of employees you want: ");
 		int n = Integer.parseInt(br.readLine());
-		
-		
+
 		String name;
 		String position;
 		float salary;
-		
+
 		Employee[] employees = new Employee[n];
-		
-		for(int i=0; i<n; i++) {
-			
+
+		for (int i = 0; i < n; i++) {
+
 			System.out.print("\nEnter name: ");
 			name = br.readLine();
-			
+
 			System.out.print("\nEnter position: ");
 			position = br.readLine();
-			
-			while(true) {
-				
+
+			while (true) {
+
 				System.out.print("\nEnter salary: ");
 				salary = Float.parseFloat(br.readLine());
-				
-				if(salary<=0) {
+
+				if (salary <= 0) {
 					System.out.print("Salary should be greater than 0");
-				}else break;
+				} else
+					break;
 			}
-			
+
 			employees[i] = new Employee(name, position, salary);
-			
+
 		}
-		
-		for(int i=0; i<n; i++) {
+
+		for (int i = 0; i < n; i++) {
 			System.out.println(employees[i]);
 		}
-		
+
 	}
-	
+
 }
